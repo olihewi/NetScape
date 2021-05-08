c37756b3c29d759b34d8871a34739e2ee6b567b4
Oli <hewisonoliver@gmail.com>
2021-05-08 16:57:11 +0100

    Per-Player UI

--- src/GameScene.cpp
+++ src/GameScene.cpp
@@ -10,32 +10,23 @@
 
 GameScene::GameScene(ASGE::Renderer* renderer, std::function<void(Scenes)> _scene_callback) :
   Scene(std::move(_scene_callback)), tile_map(renderer, "levels/dotonbori.json"),
-  players(
-    std::array<Player, 4>{ Player(renderer, ASGE::Point2D(400, 400), 0, audio_engine.get()),
-                           Player(renderer, ASGE::Point2D(500, 500), 1, audio_engine.get()),
-                           Player(renderer, ASGE::Point2D(600, 600), 2, audio_engine.get()),
-                           Player(renderer, ASGE::Point2D(700, 700), 3, audio_engine.get()) }),
-  player_cameras(
-    std::array<ASGE::Camera, 4>{ ASGE::Camera(
-                                   static_cast<float>(ASGE::SETTINGS.window_width) / 2,
-                                   static_cast<float>(ASGE::SETTINGS.window_height) / 2),
-                                 ASGE::Camera(
-                                   static_cast<float>(ASGE::SETTINGS.window_width) / 2,
-                                   static_cast<float>(ASGE::SETTINGS.window_height) / 2),
-                                 ASGE::Camera(
-                                   static_cast<float>(ASGE::SETTINGS.window_width) / 2,
-                                   static_cast<float>(ASGE::SETTINGS.window_height) / 2),
-                                 ASGE::Camera(
-                                   static_cast<float>(ASGE::SETTINGS.window_width) / 2,
-                                   static_cast<float>(ASGE::SETTINGS.window_height) / 2) })
+  players(std::array<Player, 4>{ Player(renderer, ASGE::Point2D(400, 400), 0, audio_engine.get()),
+                                 Player(renderer, ASGE::Point2D(500, 500), 1, audio_engine.get()),
+                                 Player(renderer, ASGE::Point2D(600, 600), 2, audio_engine.get()),
+                                 Player(renderer, ASGE::Point2D(700, 700), 3, audio_engine.get()) })
 {
-  for (auto& camera : player_cameras)
-  {
-    camera.setZoom(0.5F);
-  }
   for (auto& player : players)
   {
     addObject(std::make_unique<PlayerHealth>(renderer, player));
+    auto& camera = player_cameras.emplace_back(std::make_pair(
+      ASGE::Camera(
+        static_cast<float>(ASGE::SETTINGS.window_width) / 2,
+        static_cast<float>(ASGE::SETTINGS.window_height) / 2),
+      PlayerHUD()));
+    camera.first.setZoom(0.5F);
+
+    camera.second.addObject(std::make_unique<Text>(
+      renderer, "Player " + std::to_string(player.getID()), ASGE::Point2D(100, 100)));
   }
 }
 void GameScene::update(InputTracker& input, float dt)
@@ -60,7 +51,7 @@ void GameScene::update(InputTracker& input, float dt)
           other_player.getWeapon().bullet.hit_point = trace_point;
           other_player.getWeapon().bullet.has_hit   = true;
           Logging::DEBUG(
-            "Player " + std::to_string(other_player.getID()) + " hit Player " +
+            "Player " + std::to_string(other_player.getID() + 1) + " hit Player " +
             std::to_string(player.getID()) + " - " +
             std::to_string(other_player.getWeapon().bullet.damage) + " damage");
           player.takeDamage(other_player.getWeapon().bullet.damage);
@@ -77,19 +68,22 @@ void GameScene::update(InputTracker& input, float dt)
 }
 void GameScene::render(ASGE::Renderer* renderer)
 {
+  auto window_size = ASGE::Point2D(
+    static_cast<float>(ASGE::SETTINGS.window_width),
+    static_cast<float>(ASGE::SETTINGS.window_height));
   auto renderer_viewport = renderer->getViewport();
   int index              = 0;
   for (auto& camera : player_cameras)
   {
     auto player_pos = players[static_cast<size_t>(index)].centre();
-    camera.lookAt(ASGE::Point2D(
-      player_pos.x / camera.getZoom() + static_cast<float>(ASGE::SETTINGS.window_width) / 4,
-      player_pos.y / camera.getZoom() + static_cast<float>(ASGE::SETTINGS.window_height) / 4));
+    camera.first.lookAt(ASGE::Point2D(
+      player_pos.x / camera.first.getZoom() + window_size.x / 4,
+      player_pos.y / camera.first.getZoom() + window_size.y / 4));
     renderer->setViewport({ (index % 2) * ASGE::SETTINGS.window_width / 2,
                             (1 - index / 2) * ASGE::SETTINGS.window_height / 2,
                             static_cast<uint32_t>(ASGE::SETTINGS.window_width / 2),
                             static_cast<uint32_t>(ASGE::SETTINGS.window_height / 2) });
-    renderer->setProjectionMatrix(camera.getView());
+    renderer->setProjectionMatrix(camera.first.getView());
 
     Scene::render(renderer);
 
@@ -98,6 +92,9 @@ void GameScene::render(ASGE::Renderer* renderer)
     {
       player.render(renderer);
     }
+
+    renderer->setProjectionMatrix(0, 0, window_size.x / 2, window_size.y / 2);
+    camera.second.render(renderer);
 
     index++;
   }
