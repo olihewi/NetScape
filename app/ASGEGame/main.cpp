6b4290a60cde19e7652b000761de1f4f408dc64c
Oli <hewisonoliver@gmail.com>
2021-05-10 13:03:02 +0100

    Fixed linux/windows stuff hopefully

--- app/ASGEGame/main.cpp
+++ app/ASGEGame/main.cpp
@@ -1,6 +1,6 @@
 #include "ASGEGame.hpp"
 #include <Engine/Logger.hpp>
-#if _WIN32
+#if defined(_WIN32)
 #  include <wtypes.h>
 #endif
 
@@ -9,7 +9,7 @@ int main(int /*argc*/, char* /*argv*/[])
   ASGE::GameSettings game_settings;
   game_settings.window_title = "NetScape";
 
-#if _WIN32
+#if defined(_WIN32)
   RECT desktop;
   HWND h_desktop = GetDesktopWindow();
   GetWindowRect(h_desktop, &desktop);
@@ -20,7 +20,7 @@ int main(int /*argc*/, char* /*argv*/[])
   game_settings.window_height = 1080;
 #endif
 
-  game_settings.mode     = ASGE::GameSettings::WindowMode::BORDERLESS_WINDOWED;
+  game_settings.mode     = ASGE::GameSettings::WindowMode::BORDERLESS_FULLSCREEN;
   game_settings.fixed_ts = 1; // We're not using fixed ts so this is to avoid debug messages...
 
   game_settings.fps_limit  = 120;
